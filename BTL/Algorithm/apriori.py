1bbbffa88476436557492fdcd0cb883ed63f9531
Chauptm <nobitachauphong@gmai.com>
2021-04-26 23:43:53 +0700

    commit

--- BTL/Algorithm/apriori.py
+++ BTL/Algorithm/apriori.py
@@ -1,36 +1,44 @@
 import pandas as pd
 import numpy as np
+from csv import reader
 from utils import *
 from collections import defaultdict
 from itertools import permutations
 from optparse import OptionParser
-from mlxtend.preprocessing import TransactionEncoder
 
-path_to_data= "BTL/Data/groceries - groceries.csv"
-transactions= load_transactions(path_to_data)
 
-# def apriori(transactions, minSup, minConf):
-#     C1ItemSet = preprocessing(transactions);
-#     globalFreqItemSet = dict()
-#     globalItemSetWithSup = defaultdict(int)
+def aprioriFromFile(fname, minSup, minConf):
+    C1ItemSet, itemSetList = getFromFile(fname)
 
-#     L1ItemSet = getAboveMinSup( C1ItemSet, transactions, minSup, globalItemSetWithSup)
-#     currentLSet = L1ItemSet
-#     k = 2
+    # Final result global frequent itemset
+    globalFreqItemSet = dict()
+    # Storing global itemset with support count
+    globalItemSetWithSup = defaultdict(int)
 
-#     while(currentLSet):
-#         globalFreqItemSet[k-1] = currentLSet
-#         candidateSet = getUnion(currentLSet, k)
-#         candidateSet = pruning(candidateSet, currentLSet, k-1)
-#         currentLSet = getAboveMinSup(
-#             candidateSet, transactions, minSup, globalItemSetWithSup)
-#         k += 1
+    L1ItemSet = getAboveMinSup(
+        C1ItemSet, itemSetList, minSup, globalItemSetWithSup)
+    currentLSet = L1ItemSet
+    k = 2
 
-#     rules = associationRule(globalFreqItemSet, globalItemSetWithSup, minConf)
-#     rules.sort(key=lambda x: x[2])
+    # Calculating frequent item set
+    while(currentLSet):
+        # Storing frequent itemset
+        globalFreqItemSet[k-1] = currentLSet
+        # Self-joining Lk
+        candidateSet = getUnion(currentLSet, k)
+        # Perform subset testing and remove pruned supersets
+        candidateSet = pruning(candidateSet, currentLSet, k-1)
+        # Scanning itemSet for counting support
+        currentLSet = getAboveMinSup(
+            candidateSet, itemSetList, minSup, globalItemSetWithSup)
+        k += 1
 
-#     return globalFreqItemSet, rules
+    rules = associationRule(globalFreqItemSet, globalItemSetWithSup, minConf)
+    rules.sort(key=lambda x: x[2])
 
-# freqItemSet, rules = apriori(transactions, 0.03, 0.05)
-C1ItemSet= preprocessing(transactions)
-print (C1ItemSet['pip fruit'])
+    return globalFreqItemSet, rules
+
+if __name__ == "__main__":
+    fname="BTL/Data/groceries.csv"
+    freqItemSet, rules = aprioriFromFile(fname, 0.03, 0.05)
+    print(rules)
--- BTL/Algorithm/utils.py
+++ BTL/Algorithm/utils.py
@@ -1,35 +1,24 @@
-import pandas as pd
-import numpy as np
-from itertools import permutations
+from csv import reader
 from collections import defaultdict
-from mlxtend.preprocessing import TransactionEncoder
+from itertools import chain, combinations
 
-def load_transactions(path_to_data):
-    groceries = pd.read_csv(path_to_data, encoding ="utf-8")
-    groceries.head()
-    trans_df = groceries
-    del trans_df['Item(s)']
-    trans_df.fillna(0,inplace=True)
-    transactions = []
-    for i in range(0, len(trans_df)):
-        transactions.append([str(trans_df.values[i,j]) for j in range(0, 31) if str(trans_df.values[i,j])!='0'])
-    return transactions
+def powerset(s):
+    return chain.from_iterable(combinations(s, r) for r in range(1, len(s)))
 
-def preprocessing(transactions):
-    C1ItemSet= set()
-    encoder = TransactionEncoder().fit(transactions)
-    onehot = encoder.transform(transactions)
-    onehot = pd.DataFrame(onehot, columns = encoder.columns_)
-    support= onehot.mean();
-    C1ItemSet= set(support)
 
-    flattened = [i for t in transactions for i in t]
-    groceries = list(set(flattened))
+def getFromFile(fname):
+    itemSets = []
+    itemSet = set()
 
-    # Generate all possible rules
-    rules = list(permutations(groceries, 2))
-    return C1ItemSet
-
+    with open(fname, 'r') as file:
+        csv_reader = reader(file)
+        for line in csv_reader:
+            line = list(filter(None, line))
+            record = set(line)
+            for item in record:
+                itemSet.add(frozenset([item]))
+            itemSets.append(record)
+    return itemSet, itemSets
 
 
 def getAboveMinSup(itemSet, itemSetList, minSup, globalItemSetWithSup):
@@ -49,21 +38,22 @@ def getAboveMinSup(itemSet, itemSetList, minSup, globalItemSetWithSup):
 
     return freqItemSet
 
+
 def getUnion(itemSet, length):
     return set([i.union(j) for i in itemSet for j in itemSet if len(i.union(j)) == length])
+
 
 def pruning(candidateSet, prevFreqSet, length):
     tempCandidateSet = candidateSet.copy()
     for item in candidateSet:
         subsets = combinations(item, length)
         for subset in subsets:
+            # if the subset is not in previous K-frequent get, then remove the set
             if(frozenset(subset) not in prevFreqSet):
                 tempCandidateSet.remove(item)
                 break
     return tempCandidateSet
 
-def powerset(s):
-    return chain.from_iterable(combinations(s, r) for r in range(1, len(s)))
 
 def associationRule(freqItemSet, itemSetWithSup, minConf):
     rules = []
